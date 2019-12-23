cac61cde1dce8bd630dbe2a642a145774cb55cec
cottley <cottley@gmail.com>
2019-12-23 15:14:56 -0400

    Custom user class with uid from Firebase user

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,22 @@
+import 'package:beenthere/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // Create been there user object based on firebase user object
+
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
   // Method for anonymous sign in for testing
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch(err) {
       print(err.toString());
       return null;
